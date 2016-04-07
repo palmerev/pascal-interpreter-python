d7832936c5c0206042afdd69af57de86862c1535
Evan Palmer <evjpalmer@gmail.com>
2016-04-07 15:04:47 -0700

    update tests

--- test_interpreter.py
+++ test_interpreter.py
@@ -1,65 +1,82 @@
 #! /usr/bin/env python3
 
 import unittest
-from interpreter_main import Token, Interpreter, INTEGER, PLUS, EOF
-from interpreter_from_scratch import InterpreterEP, TokenEP, INT
+from interpreter_main import Token, Interpreter, Lexer, INTEGER, PLUS, EOF
+from interpreter_from_scratch import InterpreterEP, TokenEP, LexerEP, INT
 
 
 class InterpreterAdditionTestCase(unittest.TestCase):
 
     def test_single_digit_addition_no_whitespace(self):
-        self.interpreter = Interpreter("1+1")
+        self.text = "1+1"
+        self.lexer = Lexer(self.text)
+        self.interpreter = Interpreter(self.lexer)
         result = self.interpreter.expr()
-        self.assertEqual(result, 2)
+        self.assertEqual(result, eval(self.text))
 
     def test_integer_single_digit(self):
-        self.interpreter = Interpreter("1")
-        one = self.interpreter.integer()
-        self.assertEqual(one, 1)
+        self.text = "1"
+        self.lexer = Lexer(self.text)
+        self.interpreter = Interpreter(self.lexer)
+        one = self.interpreter.expr()
+        self.assertEqual(one, eval(self.text))
 
     def test_integer_double_digit(self):
-        self.interpreter = Interpreter("12")
-        twelve = self.interpreter.integer()
-        self.assertEqual(twelve, 12)
+        self.text = "12"
+        self.lexer = Lexer(self.text)
+        self.interpreter = Interpreter(self.lexer)
+        twelve = self.interpreter.expr()
+        self.assertEqual(twelve, eval(self.text))
 
     def test_integer_multidigit_plus_digit(self):
-        self.interpreter = Interpreter("123+4")
-        one = self.interpreter.integer()
-        self.assertEqual(one, 123)
+        self.text = "123+4"
+        self.lexer = Lexer(self.text)
+        self.interpreter = Interpreter(self.lexer)
+        one_twenty_seven = self.interpreter.expr()
+        self.assertEqual(one_twenty_seven, eval(self.text))
 
 
 class InterpreterSubtractionTestCase(unittest.TestCase):
-
     def test_subtraction(self):
-        self.interpreter = Interpreter("13 - 12")
+        self.text = "13 - 12"
+        self.lexer = Lexer(self.text)
+        self.interpreter = Interpreter(self.lexer)
         result = self.interpreter.expr()
-        self.assertEqual(result, 1)
+        self.assertEqual(result, eval(self.text))
 
 
 class InterpreterMultiplicationTestCase(unittest.TestCase):
     def test_multiplication(self):
-        self.interpreter = Interpreter("2 * 3")
+        self.text = "2 * 3"
+        self.lexer = Lexer(self.text)
+        self.interpreter = Interpreter(self.lexer)
         result = self.interpreter.expr()
-        self.assertEqual(result, 6)
+        self.assertEqual(result, eval(self.text))
 
 
 class InterpreterFloorDivisionTestCase(unittest.TestCase):
     def test_floor_division_no_truncation(self):
-        self.interpreter = Interpreter("4 / 2")
+        self.text = "4 / 2"
+        self.lexer = Lexer(self.text)
+        self.interpreter = Interpreter(self.lexer)
         result = self.interpreter.expr()
-        self.assertEqual(result, 2)
+        self.assertEqual(result, int(eval(self.text)))
 
     def test_floor_division_with_truncated_result(self):
-        self.interpreter = Interpreter("3 / 2")
+        self.text = " 3 / 2"
+        self.lexer = Lexer(self.text)
+        self.interpreter = Interpreter(self.lexer)
         result = self.interpreter.expr()
-        self.assertEqual(result, 1)
+        self.assertEqual(result, int(eval(self.text)))
 
 
 class InterpreterLongExpressionTestCase(unittest.TestCase):
     def test_three_term_expression(self):
-        self.interpreter = Interpreter("1 + 5 / 2")
+        self.text = "1 + 5 / 2"
+        self.lexer = Lexer(self.text)
+        self.interpreter = Interpreter(self.lexer)
         result = self.interpreter.expr()
-        self.assertEqual(result, 3)
+        self.assertEqual(result, int(eval(self.text)))
 
 
 class TokenTestCase(unittest.TestCase):
@@ -80,55 +97,62 @@ class TokenTestCase(unittest.TestCase):
 
 class InterpreterFromScratchTestCase(unittest.TestCase):
     def test_can_create_interpreter(self):
-        txt = "2*3"
-        self.interpreter = InterpreterEP(txt)
-        self.assertEqual(self.interpreter.text, txt)
+        self.txt = "2*3"
+        self.lexer = LexerEP(self.txt)
+        self.interpreter = InterpreterEP(self.lexer)
+        self.assertEqual(self.interpreter.lexer.text, self.txt)
 
     @unittest.skip
     def test_expr_returns_same_input_if_integer(self):
-        txt = "3"
-        self.interpreter = InterpreterEP(txt)
-        self.assertEqual(self.interpreter.expr(), int(txt))
+        self.txt = "3"
+        self.lexer = LexerEP(self.txt)
+        self.interpreter = InterpreterEP(self.lexer)
+        self.assertEqual(self.interpreter.expr(), int(self.txt))
 
     @unittest.skip
     def test_expr_multiplication(self):
-        txt = "2*3"
-        self.interpreter = InterpreterEP(txt)
-        self.assertEqual(self.interpreter.expr(), eval(txt))
+        self.txt = "2 * 3"
+        self.lexer = LexerEP(self.txt)
+        self.interpreter = InterpreterEP(self.lexer)
+        self.assertEqual(self.interpreter.expr(), eval(self.txt))
 
     def test_get_next_token_returns_token_of_current_char(self):
-        txt = "12"
-        self.interpreter = InterpreterEP(txt)
-        token = self.interpreter.get_next_token()
+        self.txt = "12"
+        self.lexer = LexerEP(self.txt)
+        self.interpreter = InterpreterEP(self.lexer)
+        token = self.interpreter.current_token
         self.assertIsInstance(token, TokenEP)
         self.assertEqual(token.kind, INT)
-        self.assertEqual(token.value, int(txt))
+        self.assertEqual(token.value, int(self.txt))
 
     def test_get_next_token_tokenizes_expression(self):
-        txt = "6/2"
-        self.interpreter = InterpreterEP(txt)
-        tkn1 = self.interpreter.get_next_token()
+        self.txt = "6/2"
+        self.lexer = LexerEP(self.txt)
+        self.interpreter = InterpreterEP(self.lexer)
+        tkn1 = self.interpreter.current_token
         self.assertEqual(tkn1.value, 6)
-        tkn2 = self.interpreter.get_next_token()
+        tkn2 = self.interpreter.lexer.get_next_token()
         self.assertEqual(tkn2.value, "/")
-        tkn3 = self.interpreter.get_next_token()
+        tkn3 = self.interpreter.lexer.get_next_token()
         self.assertEqual(tkn3.value, 2)
-        self.assertIsNone(self.interpreter.current_token)
 
     def test_expr_returns_single_int(self):
-        txt = "12"
-        self.interpreter = InterpreterEP(txt)
+        self.txt = "12"
+        self.lexer = LexerEP(self.txt)
+        self.interpreter = InterpreterEP(self.lexer)
         result = self.interpreter.expr()
-        self.assertEqual(result, int(txt))
+        self.assertEqual(result, int(self.txt))
 
     def test_expr_evaluates_simple_expression(self):
-        txt = "6/2"
-        self.interpreter = InterpreterEP(txt)
+        self.txt = "6/2"
+        self.lexer = LexerEP(self.txt)
+        self.interpreter = InterpreterEP(self.lexer)
         result = self.interpreter.expr()
-        self.assertEqual(result, eval(txt))
+        self.assertEqual(result, eval(self.txt))
 
     def test_expr_evaluates_long_expression(self):
-        txt = "160/2*30"
-        self.interpreter = InterpreterEP(txt)
+        self.txt = "160/2*30"
+        self.lexer = LexerEP(self.txt)
+        self.interpreter = InterpreterEP(self.lexer)
         result = self.interpreter.expr()
-        self.assertEqual(result, eval(txt))
+        self.assertEqual(result, eval(self.txt))
